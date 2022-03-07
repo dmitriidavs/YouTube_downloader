25d83b4fb27b117318657eecc53222d84601c189
deeponda <dmitriy.davletshin@bk.ru>
2022-03-07 05:26:51 +0300

    :100:

--- ytparser.py
+++ ytparser.py
@@ -1,6 +1,7 @@
 import json
 from yt_dlp import YoutubeDL
 import os
+import time
 
 class bcolors:
     HEADER = '\033[95m'
@@ -33,70 +34,74 @@ with open('yt_config.json', encoding = 'utf-8') as file:
     yt_content_dict = json.load(file)
     yt_content_dict.popitem()
 
-for content_key in yt_content_dict:
-    if content_key in ["channels", "playlists", "videos"]:
-        content_dict = yt_content_dict[content_key]
-        CONTENT_list = content_dict["content_by_name"]
-        CONTENT_dir = content_dict["config"][0]
-        CONTENT_res = content_dict["config"][1]
-    else:
-        print(f'Unsupported Content Type: {bcolors.FAIL}{content_key}{bcolors.ENDC}')
-        continue
+try:
+    for content_key in yt_content_dict:
+        if content_key in ["channels", "playlists", "videos"]:
+            content_dict = yt_content_dict[content_key]
+            CONTENT_list = content_dict["content_by_name"]
+            CONTENT_dir = content_dict["config"][0]
+            CONTENT_res = content_dict["config"][1]
+        else:
+            print(f'Unsupported Content Type: {bcolors.FAIL}{content_key}{bcolors.ENDC}')
+            continue
 
-    if CONTENT_res == "best":
-        CONTENT_res = best_format_selector
-    else:
-        CONTENT_res = f'[height <= {CONTENT_res}]'
+        if CONTENT_res == "best":
+            CONTENT_res = best_format_selector
+        else:
+            CONTENT_res = f'[height <= {CONTENT_res}]'
 
-    if content_key in ["channels", "playlists"]:
-        for key, value in CONTENT_list.items():
-            current_dir = os.path.join(CONTENT_dir, key)
-            if not os.path.exists(current_dir):
-                os.makedirs(current_dir)
+        if content_key in ["channels", "playlists"]:
+            for key, value in CONTENT_list.items():
+                current_dir = os.path.join(CONTENT_dir, key)
+                if not os.path.exists(current_dir):
+                    os.makedirs(current_dir)
 
-            print(f'\nDownloading {content_key}: {bcolors.OKGREEN}{key}{bcolors.ENDC}')
-            if value[1] != 69:
+                print(f'\nDownloading {content_key}: {bcolors.OKGREEN}{key}{bcolors.ENDC}')
+                if value[1] != 69:
+                    ydl_opts = {
+                        "format": CONTENT_res,
+                        'progress_hooks': [custom_hook],
+                        'outtmpl': f'{current_dir}/%(title)s.%(ext)s',
+                        'max_downloads': value[1]
+                    }
+                else:
+                    ydl_opts = {
+                        "format": CONTENT_res,
+                        'progress_hooks': [custom_hook],
+                        'outtmpl': f'{current_dir}/%(title)s.%(ext)s'
+                    }
+
+                try:
+                    with YoutubeDL(ydl_opts) as ydl:
+                        ydl.cache.remove()
+                        ydl.download([value[0]])
+                except KeyboardInterrupt:
+                    break
+                except:
+                    print(f'Download Error: {value[0]}, next video ...')
+
+        elif content_key == 'videos':
+            for video_url in CONTENT_list:
+                if not os.path.exists(CONTENT_dir):
+                    os.makedirs(CONTENT_dir)
+
+                print(f'\nDownloading {content_key}: {bcolors.OKGREEN}{video_url}{bcolors.ENDC}')
                 ydl_opts = {
                     "format": CONTENT_res,
                     'progress_hooks': [custom_hook],
-                    'outtmpl': f'{current_dir}/%(title)s.%(ext)s',
-                    'max_downloads': value[1]
+                    'outtmpl': f'{CONTENT_dir}/%(title)s.%(ext)s'
                 }
-            else:
-                ydl_opts = {
-                    "format": CONTENT_res,
-                    'progress_hooks': [custom_hook],
-                    'outtmpl': f'{current_dir}/%(title)s.%(ext)s'
-                }
-
-            try:
-                with YoutubeDL(ydl_opts) as ydl:
-                    ydl.cache.remove()
-                    ydl.download([value[0]])
-            except KeyboardInterrupt:
-                break
-            except:
-                print(f'Download Error: {value[0]}, next video ...')
-
-    elif content_key == 'videos':
-        for video_url in CONTENT_list:
-            if not os.path.exists(CONTENT_dir):
-                os.makedirs(CONTENT_dir)
-
-            print(f'\nDownloading {content_key}: {bcolors.OKGREEN}{video_url}{bcolors.ENDC}')
-            ydl_opts = {
-                "format": CONTENT_res,
-                'progress_hooks': [custom_hook],
-                'outtmpl': f'{CONTENT_dir}/%(title)s.%(ext)s'
-            }
-            try:
-                with YoutubeDL(ydl_opts) as ydl:
-                    ydl.cache.remove()
-                    ydl.download([video_url])
-            except KeyboardInterrupt:
-                break
-            except:
-                print(f'Download Error: {value[0]}, next video ...')
+                try:
+                    with YoutubeDL(ydl_opts) as ydl:
+                        ydl.cache.remove()
+                        ydl.download([video_url])
+                except KeyboardInterrupt:
+                    break
+                except:
+                    print(f'Download Error: {value[0]}, next video ...')
+except Exception as e:
+    print(str(e))
+    time.sleep(10)
 
 """
     если не хватит места - сами виноваты ;-)
